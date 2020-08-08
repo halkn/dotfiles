5be2df13887ed8bcb4ef665299d65b7bb97bbef3
halkn <h.mhty0wia.s@gmail.com>
2020-08-08 19:38:20 +0900

    Fix: minor update.

--- etc/update.sh
+++ etc/update.sh
@@ -15,7 +15,6 @@ GO111MODULE=off go get -u github.com/Code-Hex/battery/cmd/battery
 env GO111MODULE=on go get github.com/itchyny/gojq/cmd/gojq
 GO111MODULE=on go get mvdan.cc/sh/v3/cmd/shfmt
 go get github.com/x-motemen/ghq
-github.com/skanehira/gtran
 
 # npm tools
 npm install -g npm
