e5b06b28c43f30461218291810874dd1a35c0c25
halkn <h.mhty0wia.s@gmail.com>
2021-03-27 09:38:29 +0900

    Fix: gcloud.zsh to add gsutil with fzf.

--- .config/zsh/rc.d/gcloud.zsh
+++ .config/zsh/rc.d/gcloud.zsh
@@ -39,3 +39,32 @@ gca() {
   fi
   gcloud config configurations activate ${selected}
 }
+
+# fuzzy gsutil
+fgs() {
+  local current selected laststr cand obj
+  current="$1"
+  while true; do
+    cand=$(echo ".."; gsutil ls ${current})
+    selected=$(echo $cand| fzf -m --info=inline --header="Current: ${current}")
+    if [ -z "${selected}" ]; then
+      return 0
+    fi
+    laststr=${selected: -1}
+    if [ ${laststr} = "/" ]; then
+      current=${selected}
+    elif [ ${selected} = ".." ]; then
+      if [ "${current}" != "" ]; then
+        current=${current%/*/*}
+        if [ ${current} = "gs:" -o ${current} = "gs:/" ]; then
+          current=""
+        fi
+      fi
+    else
+      while read -r line; do
+        gsutil cp ${line} .
+      done < <(echo "${selected}")
+      return 0
+    fi
+  done
+}
