c8f3bdf13fd1de3838731999fa69c1f447f2c26e
halkn <nhalkn0920@gmail.com>
2024-12-31 11:42:03 +0900

    Improve nvim ui plugins

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,7 +1,5 @@
-local map = vim.keymap.set
-local kopts = { noremap = true, silent = true }
-
-return {
+--@type LazySpec
+local spec = {
   -- colorscheme
   {
     "EdenEast/nightfox.nvim",
@@ -11,88 +9,56 @@ return {
       vim.cmd([[colorscheme nightfox]])
     end,
   },
+
   -- status line
   {
     'nvim-lualine/lualine.nvim',
-    event = { "BufReadPre", "BufWinEnter", "BufEnter" },
+    event = { "VeryLazy" },
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      require('lualine').setup{
-        options = {
-          icons_enabled = true,
-          theme = 'nightfox',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          always_show_tabline = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 100,
-            tabline = 100,
-            winbar = 100,
-          }
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {'encoding', 'fileformat', 'filetype'},
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      }
-    end,
-  },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    cmd = "Neotree",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+    opts = {
+      options = {
+        theme = 'nightfox',
+        icons_enabled = true,
+        component_separators = { left = '|', right = '|' },
+        section_separators = { left = '', right = '' },
+        globalstatus = true,
+      },
+      tabline = { lualine_a = { 'buffers' }, lualine_z = { 'tabs' }, }
     },
-    init = function()
-        map("n", "<Leader>e", "<cmd>Neotree toggle<CR>", kopts)
-    end,
-    config = function()
-      require("neo-tree").setup({
-        window = {
-            position = "float",
-        },
-        enable_git_status = true,
-        filesystem = {
-          filtered_items = {
-            visible = true,
-            hide_dotfiles = false,
-            hide_gitignored = true,
-            hide_by_name = {
-                ".DS_Store",
-            },
-          },
-        },
-        follow_current_file = {
-            enabled = true,
-        },
-        never_show = {},
-      })
-    end
-  }
+  },
+
+  -- git
+  {
+    'lewis6991/gitsigns.nvim',
+    event = { "VeryLazy" },
+    opts = {
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ ']c', bang = true })
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({ '[c', bang = true })
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+      end
+    },
+  },
 }
+
+return spec
