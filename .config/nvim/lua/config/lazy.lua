d25592547577888de7962232a6370f3162e2993a
halkn <nhalkn0920@gmail.com>
2024-12-31 10:07:37 +0900

    Improve nvim

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -2,11 +2,18 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  local out = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    lazyrepo,
+    lazypath,
+  })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -19,23 +26,26 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   defaults = { lazy = true },
   spec = {
-    -- import your plugins
     { import = "plugins" },
   },
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
   performance = {
-      cache = { enabled = true },
-      rtp = {
-          disabled_plugins = {
-              "gzip",
-              "matchit",
-              "matchparen",
-              "netrwPlugin",
-              "rplugin",
-              "tarPlugin",
-              "tohtml",
-              "tutor",
-              "zipPlugin",
-          },
+    cache = { enabled = true },
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "rplugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
       },
+    },
   },
 })
