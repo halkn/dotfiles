7eb50089bd4e33a4c7930000eead39c536c886b0
halkn <nhalkn0920@gmail.com>
2025-01-01 16:34:20 +0900

    Improve nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -27,7 +27,7 @@ opt.signcolumn = "yes"
 opt.wrap = false
 opt.list = true
 opt.listchars = "tab:»-,extends:»,precedes:«,nbsp:%,eol:↲,trail:~"
-opt.scrolloff= 8
+opt.scrolloff = 8
 opt.termguicolors = true
 opt.background = "dark"
 opt.synmaxcol = 512
@@ -91,6 +91,34 @@ opt.lazyredraw = true
 -- grep
 -- `rg`コマンドの存在を確認し、存在する場合に`grepprg`と`grepformat`を設定
 if vim.fn.executable("rg") == 1 then
-  opt.grepprg = "rg --vimgrep"
+  opt.grepprg = 'rg --vimgrep --hidden --glob "!**/.git/*"'
   opt.grepformat = "%f:%l:%c:%m"
 end
+
+--diagnostic
+vim.diagnostic.config({
+  virtual_text = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = "󰅚",
+      [vim.diagnostic.severity.WARN] = "󰀪 ",
+      [vim.diagnostic.severity.HINT] = "󰌶 ",
+      [vim.diagnostic.severity.INFO] = "󰋽 ",
+    },
+    linehl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+    },
+    numhl = {
+      [vim.diagnostic.severity.ERROR] = 'DiagnosticSignError',
+      [vim.diagnostic.severity.WARN] = 'DiagnosticSignWarn',
+      [vim.diagnostic.severity.HINT] = 'DiagnosticSignHint',
+      [vim.diagnostic.severity.INFO] = 'DiagnosticSignInfo',
+    },
+  },
+})
