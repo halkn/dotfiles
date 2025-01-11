7afeb2d849b04f4a79246b0ef86c936a42943d38
halkn <nhalkn0920@gmail.com>
2025-01-11 10:16:41 +0900

    Improve nvim treesitter.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@
 local spec = {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
     event = { "BufReadPre", "BufNewFile" },
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
@@ -18,6 +19,38 @@ local spec = {
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
+      },
+      indent = {
+        enable = true,
+      },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+            ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+          },
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = true,
+        },
+        swap = {
+          enable = true,
+          swap_next = {
+            ["g>"] = "@parameter.inner",
+          },
+          swap_previous = {
+            ["g<"] = "@parameter.inner",
+          },
+        },
       },
     },
   }
