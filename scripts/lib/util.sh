3ec3b37e6e2497740fa83b41b1f3a589806340e3
halkn <nhalkn0920@gmail.com>
2025-01-27 20:17:04 +0900

    Add scripts utility.

--- scripts/lib/util.sh
+++ scripts/lib/util.sh
@@ -12,3 +12,18 @@ print_end() {
   echo "# #####################################################################"
   echo ""
 }
+
+install_binary() {
+  url=$1
+  bin=$2
+  install_dir=$3
+
+  print_start "${bin} install"
+  curl -LO "$url"
+
+  mkdir -p ./tmp
+  rm -fr "${install_dir:?}/"
+  tar -xzf "${bin}" --strip-components 1 -C ./tmp
+  cp -pr ./tmp "$install_dir"
+  print_end "${bin} install"
+}
