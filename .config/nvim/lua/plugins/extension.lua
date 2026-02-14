7594156f75d206ec74ceeecbb76eade0f7311cbe
halkn <nhalkn0920@gmail.com>
2026-02-14 11:03:24 +0900

    fix: nvim plugin for md.

--- .config/nvim/lua/plugins/extension.lua
+++ .config/nvim/lua/plugins/extension.lua
@@ -92,11 +92,11 @@ local spec = {
     end
   },
   {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    -- Completion for `blink.cmp`
-    dependencies = { "saghen/blink.cmp" },
-  },
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
+    ---@module 'render-markdown'
+    opts = {},
+  }
 }
 
 return spec
