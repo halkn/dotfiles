46efbe30f9550ad3063237a5d722f244919a3303
halkn <nhalkn0920@gmail.com>
2026-03-22 18:32:19 +0900

    feat: replace self-made statusline/git with lualine and gitsigns
    mini.diff/mini.git を gitsigns に、自作 statusline を lualine に置き換え。
    mini.pairs は自作 pairs モジュールに移行。

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,25 @@
+return {
+  src = 'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup({
+      on_attach = function(bufnr)
+        local gs = require('gitsigns')
+        local map = function(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+        end
+
+        -- Navigation
+        map('n', ']c', function() gs.nav_hunk('next') end, 'Next hunk')
+        map('n', '[c', function() gs.nav_hunk('prev') end, 'Prev hunk')
+        map('n', ']C', function() gs.nav_hunk('last') end, 'Last hunk')
+        map('n', '[C', function() gs.nav_hunk('first') end, 'First hunk')
+
+        -- Actions
+        map('n', '<Leader>hs', gs.stage_hunk, 'Stage hunk')
+        map('n', '<Leader>hr', gs.reset_hunk, 'Reset hunk')
+        map('n', '<Leader>hp', gs.preview_hunk, 'Preview hunk')
+        map('n', '<Leader>hb', function() gs.blame_line({ full = true }) end, 'Blame line')
+      end,
+    })
+  end
+}
--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -3,52 +3,12 @@ return {
   config = function()
     -- Text editing
     require('mini.align').setup()
-    require('mini.jump').setup()
     require('mini.operators').setup({
       replace = { prefix = 'R' },
       exchange = { prefix = 'g/' },
     })
     vim.keymap.set('n', 'RR', 'R', { desc = 'Replace mode' })
-    require('mini.pairs').setup({
-      mappings = {
-        ['('] = { action = 'open', pair = '()', neigh_pattern = '[^\\].' },
-        ['['] = { action = 'open', pair = '[]', neigh_pattern = '[^\\].' },
-        ['{'] = { action = 'open', pair = '{}', neigh_pattern = '[^\\].' },
-        ['<'] = { action = 'open', pair = '<>', neigh_pattern = '[^\\].' },
-        [')'] = { action = 'close', pair = '()', neigh_pattern = '[^\\].' },
-        [']'] = { action = 'close', pair = '[]', neigh_pattern = '[^\\].' },
-        ['}'] = { action = 'close', pair = '{}', neigh_pattern = '[^\\].' },
-        ['>'] = { action = 'close', pair = '<>', neigh_pattern = '[^\\].' },
-        ['"'] = { action = 'closeopen', pair = '""', neigh_pattern = '[^\\].', register = { cr = false } },
-        ["'"] = { action = 'closeopen', pair = "''", neigh_pattern = '[^%a\\].', register = { cr = false } },
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\].', register = { cr = false } },
-      },
-    })
-    vim.keymap.set('i', '<C-h>', '<BS>')
     require('mini.splitjoin').setup({ mappings = { toggle = '<Leader>j' } })
     require('mini.surround').setup()
-
-    -- General workflow
-    require('mini.diff').setup({
-      view = {
-        style = 'sign',
-        signs = { add = '+', change = '~', delete = '-' }
-      },
-      mappings = {
-        goto_first = '[C',
-        goto_prev = '[c',
-        goto_next = ']c',
-        goto_last = ']C',
-      }
-    })
-    require('mini.git').setup({})
-    require('mini.files').setup()
-    vim.api.nvim_create_user_command(
-      'Files',
-      function()
-        MiniFiles.open()
-      end,
-      { desc = 'Open file explorer' }
-    )
   end
 }
