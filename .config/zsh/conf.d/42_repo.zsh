6ab4df229a600c5ad5b2ed8e6836f5c8833a48b4
halkn <nhalkn0920@gmail.com>
2026-04-05 16:57:17 +0900

    style: shfmt -i 2 -ci で42_repo.zshをフォーマット

--- .config/zsh/conf.d/42_repo.zsh
+++ .config/zsh/conf.d/42_repo.zsh
@@ -1,11 +1,15 @@
 repo() {
   local root=$HOME/dev
-  local cmd="${1:-cd}"; shift
+  local cmd="${1:-cd}"
+  shift
 
   case "$cmd" in
     get)
       local url="$1"
-      [[ -z "$url" ]] && { echo "usage: repo get <url|owner/repo>" >&2; return 1; }
+      [[ -z "$url" ]] && {
+        echo "usage: repo get <url|owner/repo>" >&2
+        return 1
+      }
 
       local host owner name dest
 
@@ -14,33 +18,44 @@ repo() {
           host="dev.azure.com"
           local path="${url#*dev.azure.com/}"
           path="${path#*@dev.azure.com/}"
-          local org="${path%%/*}"; path="${path#*/}"
-          local project="${path%%/*}"; path="${path#*/}"
+          local org="${path%%/*}"
+          path="${path#*/}"
+          local project="${path%%/*}"
+          path="${path#*/}"
           path="${path#_git/}"
           name="${path%%/*}"
           dest=$root/$host/$org/$project/$name
           ;;
         https://*)
           local stripped="${url#https://}"
-          host="${stripped%%/*}"; stripped="${stripped#*/}"
-          owner="${stripped%%/*}"; stripped="${stripped#*/}"
-          name="${stripped%%/*}"; name="${name%.git}"
+          host="${stripped%%/*}"
+          stripped="${stripped#*/}"
+          owner="${stripped%%/*}"
+          stripped="${stripped#*/}"
+          name="${stripped%%/*}"
+          name="${name%.git}"
           dest=$root/$host/$owner/$name
           ;;
         git@*)
           local stripped="${url#git@}"
-          host="${stripped%%:*}"; stripped="${stripped#*:}"
-          owner="${stripped%%/*}"; name="${stripped#*/}"; name="${name%.git}"
+          host="${stripped%%:*}"
+          stripped="${stripped#*:}"
+          owner="${stripped%%/*}"
+          name="${stripped#*/}"
+          name="${name%.git}"
           dest=$root/$host/$owner/$name
           ;;
         */*)
           host="github.com"
-          owner="${url%%/*}"; name="${url#*/}"; name="${name%.git}"
+          owner="${url%%/*}"
+          name="${url#*/}"
+          name="${name%.git}"
           dest=$root/$host/$owner/$name
           url="https://github.com/$owner/$name"
           ;;
         *)
-          echo "repo get: cannot parse '$url'" >&2; return 1
+          echo "repo get: cannot parse '$url'" >&2
+          return 1
           ;;
       esac
 
@@ -53,9 +68,9 @@ repo() {
       ;;
 
     list)
-      find $root -maxdepth 5 -name ".git" -type d \
-        | sed "s|$root/||; s|/.git$||" \
-        | sort
+      find $root -maxdepth 5 -name ".git" -type d |
+        sed "s|$root/||; s|/.git$||" |
+        sort
       ;;
 
     cd)
@@ -67,7 +82,8 @@ repo() {
       ;;
 
     *)
-      echo "usage: repo <get|list|cd>" >&2; return 1
+      echo "usage: repo <get|list|cd>" >&2
+      return 1
       ;;
   esac
 }
