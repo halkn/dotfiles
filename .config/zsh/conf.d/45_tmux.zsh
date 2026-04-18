d634654772e2524799bbaca7833b6f03c45b9364
halkn <nhalkn0920@gmail.com>
2026-04-18 16:46:29 +0900

    fix: use tmux.

---
+++ .config/zsh/conf.d/45_tmux.zsh
@@ -0,0 +1,12 @@
+# ── tmux ─────────────────────────────────────────────
+export ZSH_TMUX_AUTO_START=${ZSH_TMUX_AUTO_START:-1}
+export ZSH_TMUX_SESSION_NAME=${ZSH_TMUX_SESSION_NAME:-main}
+
+if command -v tmux > /dev/null 2>&1 \
+  && [[ -o interactive ]] \
+  && [[ -z $TMUX ]] \
+  && [[ -t 0 ]] \
+  && [[ -t 1 ]] \
+  && [[ $ZSH_TMUX_AUTO_START == 1 ]]; then
+  exec tmux new-session -A -s "$ZSH_TMUX_SESSION_NAME"
+fi
