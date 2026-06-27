27b2c499b0833f87f07149c7e456918ce5a4e9ea
halkn <nhalkn0920@gmail.com>
2026-06-27 16:19:26 +0900

    fix: add force = true to xdg.configFile.nix to overwrite existing dir

--- home.nix
+++ home.nix
@@ -174,7 +174,7 @@
     "nvim".source = ./.config/nvim;
     "zsh".source = ./.config/zsh;
     "herdr".source = ./.config/herdr;
-    "nix".source = ./.config/nix;
+    "nix" = { source = ./.config/nix; force = true; };
     "yamllint".source = ./.config/yamllint;
     "yamlfmt".source = ./.config/yamlfmt;
   } // lib.optionalAttrs pkgs.stdenv.isLinux {
