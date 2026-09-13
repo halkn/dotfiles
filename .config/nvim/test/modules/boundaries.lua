0e9a3e3104f399648749bbe6f6f01b87c9e347dd
halkn <nhalkn0920@gmail.com>
2026-09-13 19:40:42 +0900

    fix: fail boundary test process on assertion

--- .config/nvim/test/modules/boundaries.lua
+++ .config/nvim/test/modules/boundaries.lua
@@ -11,29 +11,6 @@ local terminal = require('vimrc.modules.terminal')
 local yankring = require('vimrc.modules.yankring')
 local pairs_module = require('vimrc.modules.pairs')
 
-assert(rawequal(vim.ui.select, original_select))
-assert(rawequal(vim.notify, original_notify))
-
-assert(type(input.input) == 'function')
-assert(type(notify.notify) == 'function')
-notify.setup()
-pairs_module.setup()
-picker.setup()
-explorer.setup()
-replace.setup()
-surround.setup()
-terminal.setup()
-yankring.setup()
-
-assert(rawequal(vim.ui.select, original_select))
-assert(rawequal(vim.notify, original_notify))
-for _, lhs in ipairs({ 'sa', 'sd', 'sr', 'R', 'p', 'P', 'gp', 'gP', '<C-p>', '<C-n>' }) do
-  assert(vim.fn.maparg(lhs, 'n') == '', 'unexpected normal mapping: ' .. lhs)
-end
-assert(vim.fn.maparg('<Leader>f', 'n') == '')
-assert(vim.fn.maparg('<Leader>e', 'n') == '')
-assert(vim.fn.maparg('<C-t>', 'n') == '')
-
 local function feed(keys)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'x', false)
 end
@@ -45,27 +22,58 @@ local function scratch(lines)
   vim.api.nvim_win_set_cursor(0, { 1, 0 })
 end
 
-surround.setup({ mappings = { add = 'za' } })
-scratch({ 'word' })
-feed('zaiw"')
-assert(vim.api.nvim_get_current_line() == '"word"')
+local function run()
+  assert(rawequal(vim.ui.select, original_select))
+  assert(rawequal(vim.notify, original_notify))
 
-replace.setup({ mappings = { replace = 'Q' } })
-scratch({ 'word' })
-vim.fn.setreg('"', 'replacement')
-feed('Qiw')
-assert(vim.api.nvim_get_current_line() == 'replacement')
+  assert(type(input.input) == 'function')
+  assert(type(notify.notify) == 'function')
+  notify.setup()
+  pairs_module.setup()
+  picker.setup()
+  explorer.setup()
+  replace.setup()
+  surround.setup()
+  terminal.setup()
+  yankring.setup()
 
-pairs_module.setup({ mappings = { quotes = { '~' } } })
-scratch({ '' })
-feed('i~<Esc>')
-assert(vim.api.nvim_get_current_line() == '~~')
+  assert(rawequal(vim.ui.select, original_select))
+  assert(rawequal(vim.notify, original_notify))
+  for _, lhs in ipairs({ 'sa', 'sd', 'sr', 'R', 'p', 'P', 'gp', 'gP', '<C-p>', '<C-n>' }) do
+    assert(vim.fn.maparg(lhs, 'n') == '', 'unexpected normal mapping: ' .. lhs)
+  end
+  assert(vim.fn.maparg('<Leader>f', 'n') == '')
+  assert(vim.fn.maparg('<Leader>e', 'n') == '')
+  assert(vim.fn.maparg('<C-t>', 'n') == '')
 
-yankring.setup({ mappings = { paste_after = 'zP' } })
-scratch({ 'source', 'target' })
-vim.cmd('normal! y$')
-vim.api.nvim_win_set_cursor(0, { 2, 5 })
-feed('zP')
-assert(vim.api.nvim_get_current_line() == 'targetsource')
+  surround.setup({ mappings = { add = 'za' } })
+  scratch({ 'word' })
+  feed('zaiw"')
+  assert(vim.api.nvim_get_current_line() == '"word"')
 
+  replace.setup({ mappings = { replace = 'Q' } })
+  scratch({ 'word' })
+  vim.fn.setreg('"', 'replacement')
+  feed('Qiw')
+  assert(vim.api.nvim_get_current_line() == 'replacement')
+
+  pairs_module.setup({ mappings = { quotes = { '~' } } })
+  scratch({ '' })
+  feed('i~<Esc>')
+  assert(vim.api.nvim_get_current_line() == '~~')
+
+  yankring.setup({ mappings = { paste_after = 'zP' } })
+  scratch({ 'source', 'target' })
+  vim.cmd('normal! y$')
+  vim.api.nvim_win_set_cursor(0, { 2, 5 })
+  feed('zP')
+  assert(vim.api.nvim_get_current_line() == 'targetsource')
+end
+
+local ok, err = xpcall(run, debug.traceback)
+if not ok then
+  io.stderr:write(err .. '\n')
+  vim.cmd('cquit 1')
+end
+io.write('boundaries test passed\n')
 vim.cmd('qa!')
